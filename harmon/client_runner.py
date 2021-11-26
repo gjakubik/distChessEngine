e670300a848757fff1a99e78be884371b4d83ca7
Michael Lee <mlee43@nd.edu>
2021-11-26 14:50:46 -0500

    client should (?) be good to test

--- harmon/client_runner.py
+++ harmon/client_runner.py
@@ -8,14 +8,18 @@ import game_client
 import select
 import json
 
+# globals
+DEPTH = 10 # num turns to sim
+ENGINE_TIME = 3 # seconds
 
 def main():
-    stockfish = Stockfish("C:\\Users\\micha\Downloads\\stockfish_14.1_win_x64_avx2\\stockfish_14.1_win_x64_avx2\\stockfish_14.1_win_x64_avx2.exe")
-
-    # TODO parse argv
-    project = sys.argv[1]
-    owner = sys.argv[2]
-    k = int(sys.argv[3])
+    # parse argv
+    stockfish_path = sys.argv[1]
+    project = sys.argv[2]
+    owner = sys.argv[3]
+    k = int(sys.argv[4])
+    stockfish = Stockfish(stockfish_path)
+    #"C:\\Users\\micha\Downloads\\stockfish_14.1_win_x64_avx2\\stockfish_14.1_win_x64_avx2\\stockfish_14.1_win_x64_avx2.exe"
 
     master_client = game_client.GameClient(project, owner, 'master', k, 0, stockfish)
     response = master_client.game_server_connect(project)
@@ -26,16 +30,27 @@ def main():
 
     for i in range(k):
         worker = game_client.GameClient(project, owner, 'worker', k, i+1, stockfish)
+        worker.game_server_connect(project) # open connection to server for each worker, only gets used if master fails
+        worker.worker.connect((master_client.host, master_client.port))
         master_client.workers.append(worker)
     
     
     inputs = [ worker.worker for worker in master_client.workers ] + [ master_client.listener ]
     print(inputs)
-    outputs = [ ]
+    #outputs = [ worker.worker for worker in master_client.workers ]
     while True:
         try:
+            # check for a new master
+            for worker in master_client.workers:
+                if worker.role == 'master':
+                    master_client = worker
+                    inputs = [worker.worker for worker in master_client.workers] + [master_client.listener]
+                    #outputs = outputs = [ worker.worker for worker in master_client.workers ]
+                    break
+
             # master checks for worker connections
-            # master listens for message from game server and handles it if needed
+            # master listens for message from name server and handles it if needed
+            # in this block, have master client update the game server with current list of workers
             if (time.time() - master_client.last_update) >= 60:
                 master_client.last_update = master_client.update_ns(project)
             readable, writeable, exceptional = select.select(inputs, outputs, inputs)
@@ -65,31 +80,46 @@ def main():
                             # pick a move
                             board_state = message['board_state'] # fen string of the board state 
                             master_client.stockfish.set_fen_position(board_state)
-                            moves = master_client.gen_moves()
+                            moves = master_client.gen_moves() # generate moves
                             print(f'moves: {moves}')
                             # send messages to all the workers
                             if master_client.workers:
-                                # TODO handle the workers
-                                pass
+                                # assign moves
+                                evals = [] # will be list of tuples of move and move's eval
+                                for move in moves and worker in master_client.workers:
+                                    response = master_client.assign_move(message['color'], message['board_state'], move, worker)
+                                    if response['status'] != 'OK':
+                                        # TODO handle failed client
+                                    (move, evaluation) = worker.eval_move(message['board_state'], move, DEPTH, ENGINE_TIME)
+                                    evals.append((move, evaluation)) 
+                                # evaluate the worker responses
+                                move = master_client.eval_responses(evals)[0] # [1] is the evaluation metric, may want it for stats 
                             else: # otherwise there's only the master, so we just use the best move from initial guess
                                 move = moves[0]
-                                master_client.stockfish.make_moves_from_current_position([move])
-                                message = {
-                                    'owner': 'master',
-                                    'engineId': master_client.engine_id,
-                                    'game_id': master_client.game_id,
-                                    'type': 'board_state',
-                                    'board_state': master_client.stockfish.get_fen_position()
-                                }
-                                print(f'Message: \n {message}')
-                                response = master_client.send(master_client.game_server, message)
-                                if response == None:
-                                    # TODO handle dead game server
-                                    pass
+                            # make move and respond to server
+                            master_client.stockfish.make_moves_from_current_position([move])
+                            message = {
+                                'owner': 'master',
+                                'engineId': master_client.engine_id,
+                                'game_id': master_client.game_id,
+                                'type': 'board_state',
+                                'board_state': master_client.stockfish.get_fen_position()
+                            }
+                            print(f'Message: \n {message}')
+                            response = master_client.send(master_client.game_server, message)
+                            if response == None:
+                                # TODO handle dead game server
+                                pass
+                    elif messageOwner == 'worker':
+                        print("received message from a worker")
+                        print(message)
+
+            for s in writeable: 
+                pass
             for s in exceptional:
                 inputs.remove(s)
                 s.close()
-
+            outputs = []
         except KeyboardInterrupt:
             for s in inputs:
                 s.close()
