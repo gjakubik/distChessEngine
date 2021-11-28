077ec796d7eb09fbaab733136108855a83ec95df
Gavin Jakubik <gjakubik@nd.edu>
2021-11-28 12:46:15 -0800

    trying new stuff

--- fischer/src/index.js
+++ fischer/src/index.js
@@ -4,7 +4,7 @@ const handleConnection = (conn) => {
     var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
     console.log('new client connection from %s', remoteAddress);
     const onConnData = (d) => {
-        const newData = decode(d);
+        const newData = d.decode();
         console.log('connection data from %s: %j', remoteAddress, d);
         console.log('connection data from %s: %s', remoteAddress, newData);
         conn.write(len(d))  
