e75cffcb7b66261d13b998112d5e67099f32444e
Michael Lee <mlee43@nd.edu>
2021-11-29 15:21:47 -0500

    god i hope my stockfish playing forward code works

--- harmon/game_client.py
+++ harmon/game_client.py
@@ -117,20 +117,21 @@ class GameClient:
 
     def eval_move(self, board_state, move, depth, time):
         # TODO have stockfish play forward from the board state for some # of moves and report evaluation of it
-        worker_fish = Stockfish()
-        worker_fish.set_fen_position(board_state)
-        worker_fish.make_moves_from_current_position([move])
+        self.stockfish.set_fen_position(board_state)
+        self.stockfish.make_moves_from_current_position([move])
         for i in range(depth):
-            move = worker_fish.get_best_move_time(time)
+            move = self.stockfish.get_best_move_time(time)
+            print(f'Move: {move}')
             if move == None:
                 break
-            worker_fish.make_moves_from_current_position([move])
-            move = worker_fish.get_best_move_time(time)
+            self.stockfish.make_moves_from_current_position([move])
+            move = self.stockfish.get_best_move_time(time)
+            print(f'Move: {move}')
             if move == None:
                 break
-            worker_fish.make_moves_from_current_position([move])
-
-        evaluation = worker_fish.get_evaluation()
+            self.stockfish.make_moves_from_current_position([move])
+            print(f'Current fen string: {self.stockfish.get_fen_position}')
+        evaluation = self.stockfish.get_evaluation()
         message = {'type': 'evaluation', 'move': move, 'engineId': self.engineId, 'id': id, 'move': move, 'evaluation': evaluation}
         return self.send(self.worker, message)
 
