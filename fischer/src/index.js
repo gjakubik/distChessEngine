f0909605bdb1246c0350c8ef025868939c1542db
gjakubik <gavito8800@gmail.com>
2021-11-29 21:32:40 -0500

    more changes

--- fischer/src/index.js
+++ fischer/src/index.js
@@ -1,14 +1,20 @@
-var net = require('net');
+const net = require('net');
+const APIReq = require('./utils/APIReq');
 
 const handleConnection = (conn) => {    
     var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
     console.log('new client connection from %s', remoteAddress);
+    conn.setEncoding('utf8');
+    // New connection, register new enginge
     const onConnData = (d) => {
-        const newData = d.toString('utf-8');
+        const newData = JSON.parse(d);
         console.log('connection length from %s: %d', remoteAddress, d.length);
-        console.log('connection data from %s: %s', remoteAddress, newData);
+        console.log('connection data from %s: ', remoteAddress, newData);
         console.log('connection sending length to %s: %s', remoteAddress, d.length.toString().padEnd((64 - d.length.toString().length), "*"));
-        conn.write(d.length.toString().padEnd((64 - d.length.toString().length), " "));  
+
+        // Send the request to the API
+        resp = await APIReq(newData['endpoint'], newData['method'], newData);
+        conn.write(JSON.stringify(resp).length);  
         conn.write(d);  
     }
 
@@ -28,7 +34,7 @@ const handleConnection = (conn) => {
 var server = net.createServer();    
 server.on('connection', handleConnection);
 
-server.listen(5050, () => {    
+server.listen(5051, () => {    
     console.log('server listening to %j', server.address());  
 });
 
