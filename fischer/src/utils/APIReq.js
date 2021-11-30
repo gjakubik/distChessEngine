8def84e746327a53a4d6ae04c5c5686147b0e8d7
gjakubik <gavito8800@gmail.com>
2021-11-29 23:12:07 -0500

    trying new agent thing

--- fischer/src/utils/APIReq.js
+++ fischer/src/utils/APIReq.js
@@ -2,6 +2,10 @@ const { API_BASE_URL } = require('../constants');
 const fetch = require('cross-fetch');
 
 const APIReq = async (endpoint, method, message) => {
+
+    const httpsAgent = new https.Agent({
+        rejectUnauthorized: false,
+    });
     // function to register server
     const resp = fetch(API_BASE_URL + endpoint, {
         method: method,
@@ -9,7 +13,8 @@ const APIReq = async (endpoint, method, message) => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(message)
+        body: JSON.stringify(message),
+        agent: httpsAgent
     });
 
     if (!resp.ok) {
