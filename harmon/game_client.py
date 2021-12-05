d9e10d9fed8a03072918077f215de8529519df35
Michael Lee <mlee43@nd.edu>
2021-12-05 13:36:47 -0500

    hi

--- harmon/game_client.py
+++ harmon/game_client.py
@@ -221,7 +221,7 @@ class GameClient:
     def update_ns(self):
         # update the name server with this client's info
         # role and id are stored in type so that we can super easily figure out winner of elections
-        message = {'type': f'chessEngine-{self.role}-{self.id}', 'owner': self.owner, 'port': self.port, 'project': self.project}
+        message = {'type': f'chessEngine-{self.role}-{self.id}', 'owner': self.owner, 'port': self.worker.getsockname()[1], 'project': self.project}
         message = json.dumps(message)
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.sendto(message.encode(ENCODING), (NAME_SERVER, NS_PORT))
