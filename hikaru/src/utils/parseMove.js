9243d1c9b06d58384038e9a8d8545717c577c4b8
gjakubik <gavito8800@gmail.com>
2021-12-05 00:02:43 -0500

    fix

--- hikaru/src/utils/parseMove.js
+++ hikaru/src/utils/parseMove.js
@@ -32,29 +32,4 @@ const create = async (gameId, state, moveNum) => {
     }
 };
 
-try {
-    const myNewObject = new Parse.Object('Game');
-    myNewObject.set('username', username);
-    console.log(username, engine1Id);
-    const Server = Parse.Object.extend('Server');
-    console.log(engine1Id)
-    const query = new Parse.Query(Server);
-    const engine1Obj = await query.get(engine1Id);
-
-    console.log(engine1Obj);
-    if (engine1Obj) {
-        console.log("Found server");
-        myNewObject.set('engine1', engine1Obj.toPointer());
-    } else {
-        console.log("Failed to find server");
-        return "";
-    }
-    
-    const result = await myNewObject.save();
-    console.log(result);
-    return result.id;
-} catch (error) {
-    console.log(error);
-    return "";
-}
 module.exports = { create }
